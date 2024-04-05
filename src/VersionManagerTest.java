7e5a0898250c4c2d6a187dde7e8f1f218c259e4f
Philippe SKOPAL <pskopal@nevaone.int>
2024-04-05 10:07:03 +0200

    Setup ready

---
+++ src/VersionManagerTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VersionManagerTest {
+    private static VersionManager instance = null;
+
+    @BeforeAll
+    static void beforeAll() {
+        instance = new VersionManager();
+    }
+
+    @BeforeEach
+    void setUp() {
+        instance.setVersion(1);
+    }
+
+    @Test
+    @DisplayName("Test initialisation à 1")
+    void testInit() {
+        assertEquals(1, instance.getVersion());
+    }
+}
